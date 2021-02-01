367fc741bfa0099177866606f6d1299e8d08baf8
elco45 <dennischeong2003@yahoo.es>
2021-02-01 13:20:48 -0500

    Adding user creation logic with mongoose

---
+++ auth/src/models/user.ts
@@ -0,0 +1,49 @@
+import mongoose from 'mongoose';
+import { Password } from '../services/password';
+
+// Interface required to create new User
+interface UserAttrs {
+  email: string;
+  password: string;
+}
+
+// Interface that describes properties for User Model
+interface UserModel extends mongoose.Model<UserDoc> {
+  build(attrs: UserAttrs): UserDoc;
+}
+
+// Interface that describes properties for a User Document
+interface UserDoc extends mongoose.Document {
+  email: string;
+  password: string;
+  //   createdAt: string;
+  //   updatedeAt: string;
+}
+
+const userSchema = new mongoose.Schema<UserDoc>({
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+userSchema.pre('save', async function (done) {
+  if (this.isModified('password')) {
+    const hashed = await Password.toHash(this.get('password'));
+    this.set('password', hashed);
+  }
+  done();
+});
+
+// Custom function with typescript check
+userSchema.statics.build = (attrs: UserAttrs) => {
+  return new User(attrs);
+};
+
+const User = mongoose.model<UserDoc, UserModel>('User', userSchema);
+
+export { User };
